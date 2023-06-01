0f05ca97fb05c42c1d3eb965076636105a95f019
AMeresii <ameresii@stream.ru>
2023-06-01 10:24:09 +0500

    add allure-go files

---
+++ example/failure_examples/example_panic_handling_test.go
@@ -0,0 +1,13 @@
+package failure_examples
+
+import (
+	"github.com/AleksandrWanted/allure-go"
+	"testing"
+)
+
+func TestPanicBasic(t *testing.T) {
+	allure.Test(t, allure.Description("Panic handling test"),
+		allure.Action(func() {
+			panic("throwing a panic")
+		}))
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/AleksandrWanted/allure-go
+
+go 1.18
+
+require (
+	github.com/fatih/camelcase v1.0.0
+	github.com/jtolds/gls v4.20.0+incompatible
+	github.com/pkg/errors v0.8.1
+)
