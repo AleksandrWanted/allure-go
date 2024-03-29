e27af36dba5481c535732b258071ca372d4d493d
AleksandrWanted <52817766+AleksandrWanted@users.noreply.github.com>
2024-03-29 14:18:48 +0500

    fix concurrent map read/write on getCurrentTestPhaseObject func

--- init.go
+++ init.go
@@ -11,7 +11,7 @@ var (
 	resultsPath      string
 	createFolderOnce sync.Once
 	copyEnvFileOnce  sync.Once
-	testPhaseObjects map[string]*testPhaseContainer
+	testPhaseObjects sync.Map
 )
 
 const (
@@ -25,5 +25,5 @@ const (
 
 func init() {
 	ctxMgr = gls.NewContextManager()
-	testPhaseObjects = make(map[string]*testPhaseContainer)
+	testPhaseObjects = sync.Map{}
 }
